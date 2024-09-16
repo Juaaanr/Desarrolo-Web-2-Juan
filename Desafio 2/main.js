695c677dc3046c0acebac2fc3b5bc540bfe9d726
Juan <juanmanuel2008mail@gmail.com>
2024-09-16 20:09:03 -0300

    desafio 2

---
+++ Desafio 2/main.js
@@ -0,0 +1,11 @@
+let nombreProducto = "canelones"
+
+let precioUnitario = 599
+
+let cantidadDeseada = prompt("Cuantos canelones vas a llevar:")
+
+let costoTotal = precioUnitario * cantidadDeseada
+
+alert("el costo total es de $" + costoTotal)
+
+
